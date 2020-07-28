d75532d6f8228763d363d031af333013b6bc5992
Hongbo <zhzephizhao@huangbaoche.com>
2020-07-28 13:51:31 +0800

    底部菜单栏

--- app/src/main/java/tk/hb/english/FirstFragment.kt
+++ app/src/main/java/tk/hb/english/FirstFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -33,9 +32,14 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        //底部菜单
+        bottomAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.bottomGo -> {
+                    findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+                }
+            }
+            true
         }
     }
 
